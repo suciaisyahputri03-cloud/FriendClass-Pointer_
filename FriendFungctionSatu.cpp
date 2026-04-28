2ac7f3ab8d06c98e9f6c088c977ada2810dba86d
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-28 19:21:13 +0700

    menambahkan iimplementasi fungsi setNama untuk mengisi atribut nama

--- FriendFungctionSatu.cpp
+++ FriendFungctionSatu.cpp
@@ -6,6 +6,18 @@ class mahasiswa
 private:
     string nama;
 public:
-    friend void setName(mahasiswa &a, string);
+    friend void setNama(mahasiswa &a, string);
 };
 
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "kairi kumar");
+    return 0;
+}
