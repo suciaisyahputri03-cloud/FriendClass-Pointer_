6ad7f58ec7b8c288071715e2fe993351c07f6dde
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-28 19:40:22 +0700

    menambahkan implementasi akses member class menggunakan objek, reference, dan pointer

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -7,3 +7,16 @@ class mahasiswa{
         cout <<"No Induk = "<<nim<<endl;
     }
 };
+
+int main (){
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); //member acces opertor
+
+    mahasiswa &refMhs = mhs; // pointer reference refMhs
+    refMhs.nim = 2; // member acces operatoe
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // pointer deference pMhs
+    pMhs->nim = 3; //arrow operator
+    pMhs->showNim();
+}
